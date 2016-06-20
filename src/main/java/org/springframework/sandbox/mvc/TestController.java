451156407f44fa83e67ca9a0cb3a38a5ab81f29c
Aubergines <chinazhaoshuangquan@gmail.com>
2016-06-20 23:50:55 +0800

    测试唯医API接口

--- src/main/java/org/springframework/sandbox/mvc/TestController.java
+++ src/main/java/org/springframework/sandbox/mvc/TestController.java
@@ -1,14 +1,18 @@
 package org.springframework.sandbox.mvc;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.zsq.redis.RedisObject;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/hello")
@@ -16,6 +20,7 @@ public class TestController {
 
 
     public static final String REST_SERVICE_URI = "http://localhost:8089/";
+    public static final String ALLINMD_SERVICE_URI = "http://android1.api.allinmd.cn:18080/services/";
 
 
 
@@ -47,6 +52,48 @@ public class TestController {
         Object o = jsonObject.get("areasExpertise");
         System.out.println(o);
         return value;
+    }
+
+    @RequestMapping(value="/getToken", produces = "application/json; charset=utf-8")
+    public @ResponseBody String getTokenJSON(HttpServletRequest request) {
+        RestTemplate restTemplate = new RestTemplate();
+        String o = restTemplate.getForObject(ALLINMD_SERVICE_URI + "qiniu/storage/v2/getToken",String.class);
+
+        System.out.println(o);
+        return o;
+    }
+
+    @RequestMapping(value="/hotList", produces = "application/json; charset=utf-8")
+    public @ResponseBody String getHotListJSON(HttpServletRequest request) {
+        RestTemplate restTemplate = new RestTemplate();
+        Map paramMap = new HashMap();
+        ResponseEntity forEntity = restTemplate.getForEntity(ALLINMD_SERVICE_URI + "log/customer/keyword/v2/getHotList/", String.class, paramMap);
+
+        String o = restTemplate.getForObject(ALLINMD_SERVICE_URI + "qiniu/storage/v2/getToken",String.class);
+
+        System.out.println(o);
+        return JSON.toJSONString(forEntity);
+    }
+
+    @RequestMapping(value="/keyword", produces = "application/json; charset=utf-8")
+    public @ResponseBody String getKeywordJSON(HttpServletRequest request) {
+        RestTemplate restTemplate = new RestTemplate();
+        Map paramMap = new HashMap();
+        paramMap.put("firstResult",0);
+        paramMap.put("visitSiteId",6);
+        paramMap.put("isValid",1);
+        paramMap.put("pageSize",10);
+        paramMap.put("sortType",1);
+        paramMap.put("pageIndex",1);
+        paramMap.put("maxResult",10);
+        paramMap.put("searchParam","关节");
+        paramMap.put("treeLevel","2_3");
+        ResponseEntity forEntity = restTemplate.getForEntity(ALLINMD_SERVICE_URI + "comm/data/tag/v2/getMapList/", String.class, paramMap);
+
+        String o = restTemplate.getForObject(ALLINMD_SERVICE_URI + "qiniu/storage/v2/getToken",String.class);
+
+        System.out.println(o);
+        return JSON.toJSONString(forEntity);
     }
 
 }
