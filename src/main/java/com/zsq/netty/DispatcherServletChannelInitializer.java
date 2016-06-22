f3c72ff58892148c2a56a1630d4592d5d31a6f62
Aubergines <chinazhaoshuangquan@gmail.com>
2016-06-22 23:44:25 +0800

    测试唯医API接口

--- src/main/java/com/zsq/netty/DispatcherServletChannelInitializer.java
+++ src/main/java/com/zsq/netty/DispatcherServletChannelInitializer.java
@@ -28,27 +28,13 @@ public class DispatcherServletChannelInitializer extends ChannelInitializer<Sock
 		wac.setServletConfig(servletConfig);
 		wac.register(AppConfig.class);
 		wac.refresh();
-
 		this.dispatcherServlet = new DispatcherServlet(wac);
 		this.dispatcherServlet.init(servletConfig);
-
-		//set spring config in xml
-		//this.dispatcherServlet = new DispatcherServlet();
-		//this.dispatcherServlet.setContextConfigLocation("classpath*:/applicationContext.xml");
-		//this.dispatcherServlet.init(servletConfig);
 	}
 
 	@Override
 	public void initChannel(SocketChannel channel) throws Exception {
-		// Create a default pipeline implementation.
 		ChannelPipeline pipeline = channel.pipeline();
-
-		// Uncomment the following line if you want HTTPS
-		//SSLEngine engine = SecureChatSslContextFactory.getServerContext().createSSLEngine();
-		//engine.setUseClientMode(false);
-		//pipeline.addLast("ssl", new SslHandler(engine));
-
-
 		pipeline.addLast("decoder", new HttpRequestDecoder());
 		pipeline.addLast("aggregator", new HttpObjectAggregator(65536));
 		pipeline.addLast("encoder", new HttpResponseEncoder());
